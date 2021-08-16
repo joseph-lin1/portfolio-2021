bc03362a1f77acaaf8da15be997805d8050beb39
Joseph <josephlin550@gmail.com>
2021-08-16 13:21:56 -0700

    pushing updated package.json

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <h1>test</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
